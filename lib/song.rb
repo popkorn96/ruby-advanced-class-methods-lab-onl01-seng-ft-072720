90c91a3eedc619f99454d00464c51288bf85bb68
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-07 02:08:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,6 +38,7 @@ class Song
   def self.new_from_filename(filename)
       song = self.new
       song.name = (filename.split(" - ")[1].chomp(.mp3))
+      song.artist_name = (filename.split(" - ")[0])
     end
   end
       
