a036a3d9390580d6e4a3f1a7b08a0894d8c7c293
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-07 01:39:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,33 @@ class Song
   def self.all
     @@all
   end
-
+  def self.create
+    song = self.new 
+    song.save
+    song
+  end
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name
+    song
+  end 
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil 
+      self.create_by_name(name)
+    else 
+      self.find_by_name(name)
+    end
+    self.find_by_name(name) || self.create_by_name(name)
+  end
   def save
     self.class.all << self
   end
